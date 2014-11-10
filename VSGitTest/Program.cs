ee7bbe3e016378fb8e55d34b9d8efb466a079b20
fengyan <eflyfeng@163.com>
2014-11-10 15:52:17 +0800

    添加项目

---
+++ VSGitTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSGitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("123");
+
+            Console.ReadLine();
+        }
+    }
+}
